2e06917e8e5a2e058023890a4d29e2a4ce211f49
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-22 14:00:16 +0100

    superpoint image builds

--- deploy/install_packages.sh
+++ deploy/install_packages.sh
@@ -4,4 +4,4 @@
 apt -y install python3 python3-pip
 apt -y install libjpeg-dev zlib1g-dev
 pip3 install tqdm pillow numpy matplotlib scipy
-pip3 install pytorch torchvision 
+pip3 install torch torchvision 
