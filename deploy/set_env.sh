ef49f159a130f82eef3d0a00a82e6f8548ecb1c2
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-22 16:57:21 +0100

    testing

--- deploy/set_env.sh
+++ deploy/set_env.sh
@@ -3,5 +3,5 @@
 . /usr/home/ws/devel/setup.bash
 host_ip=$(ip route | awk '/default/ {print $3}')
 export ROS_MASTER_URI=http://${host_ip}:11311
-export ROS_HOSTNAME=localhost
+export ROS_IP=0.0.0.0
 exec "$@"
