1f8d8ff3cfeb087e062bf01caeeec0e1b5392960
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-22 16:32:48 +0100

    wip test

--- deploy/install_catkin.sh
+++ deploy/install_catkin.sh
@@ -2,4 +2,4 @@
 
 # Install required dependencies
 git clone https://github.com/catkin/catkin_simple.git --recursive
-git clone https://github.com/ethz-asl/maplab_msgs.git --recursive
+git clone -b devel/external_features https://github.com/ethz-asl/maplab_msgs.git --recursive
--- deploy/install_ros.sh
+++ deploy/install_ros.sh
@@ -5,3 +5,4 @@ sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add - 2> /dev/null
 apt -y update
 apt -y install ros-melodic-ros-base python-catkin-tools
+apt -y install ros-melodic-cv-bridge
